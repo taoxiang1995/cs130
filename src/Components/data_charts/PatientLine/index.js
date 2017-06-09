5d3d6218f076423d5e50aa1a0622355fa732afb8
taoxiang1995 <taoxiang@g.ucla.edu>
2017-06-09 01:28:45 -0700

    patient record show record

---
+++ src/Components/data_charts/PatientLine/index.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import {Line} from 'react-chartjs-2';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import '../style.css';
+
+// const data = {
+//     labels: ["January", "February", "March", "April", "May", "June", "July"],
+//     datasets: [
+//         {
+//             label: "My First dataset",
+//             fill: false,
+//             lineTension: 0.1,
+//             backgroundColor: "rgba(75,192,192,0.4)",
+//             borderColor: "rgba(75,192,192,1)",
+//             borderCapStyle: 'butt',
+//             borderDash: [],
+//             borderDashOffset: 0.0,
+//             borderJoinStyle: 'miter',
+//             pointBorderColor: "rgba(75,192,192,1)",
+//             pointBackgroundColor: "#fff",
+//             pointBorderWidth: 1,
+//             pointHoverRadius: 5,
+//             pointHoverBackgroundColor: "rgba(75,192,192,1)",
+//             pointHoverBorderColor: "rgba(220,220,220,1)",
+//             pointHoverBorderWidth: 2,
+//             pointRadius: 1,
+//             pointHitRadius: 10,
+//             data: [65, 59, 80, 81, 56, 55, 40],
+//             spanGaps: false,
+//         }
+//     ]
+// }
+
+
+// const options={}
+
+const data = {
+    // labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            spanGaps: false,
+        },
+    ]
+}
+
+
+const options={
+    maintainAspectRatio: false, 
+    scales: {
+        xAxes: [{
+            type: 'time',
+            position: 'bottom'
+        }]
+    }
+}
+
+
+class BloodStatLineChart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: null,
+            endDate: null,
+        };
+    };
+
+    render() {
+        return (
+            <div className="RectangleBackground">
+                <p className="Title">
+                    <div>{this.props.title}</div>
+                    <div style={
+                        {
+                            "display":"flex",
+                            "flexDirection": "row",
+                            "paddingLeft": "2%",
+                            "alignItems": "center",
+                        }}>
+                        <DateRangePicker
+                            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+                            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                        />
+                    </div>
+                </p>
+
+                <div className="Chart">
+                    <Line data={{
+                        datasets:[
+                            Object.assign({}, data.datasets[0], {
+                                data: this.props.data,
+                            })
+                        ]
+                    }} options={options}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BloodStatLineChart;
--- src/Pages/PatientOverView/index.js
+++ src/Pages/PatientOverView/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import BloodStatLineChart from '../../Components/data_charts/line';
+import BloodStatLineChart from '../../Components/data_charts/PatientLine';
 import DoctorUpdateForm from '../../Components/doctor_update';
 import NewRecordCard from '../../Components/new_record_card';
 import LargeNameCard from '../../Components/large_patient_card';
@@ -16,11 +16,7 @@ class PatientOverView extends Component {
     constructor(props){
         super(props);
         this.state={
-            patientInfo: {},
-            bloodSugerData: {},
-            bloodPressureData:{},
-            bloodFatData: {},
-            uniqueID:0
+            patient_info:{}
         }
     }
 
@@ -151,7 +147,9 @@ class PatientOverView extends Component {
             headers: {'Authorization': 'Bearer ' + sessionStorage.getItem('token')},
             })
             .then((response)=>{
-                debugger;
+                this.setState({
+                    patient_info:response.data.data.response
+                })
             })
             .catch(function(error) {});
     }
@@ -164,74 +162,98 @@ class PatientOverView extends Component {
         
     }
 
+    formatData(items, fieldName){
+        return items.map(item=>{
+            return {
+                x:item.created_at,
+                y:item[fieldName]
+            }
+        })
+    }
 
     render() {
-        return (
-            <div className="Page">
-                <SideBar/>
+        debugger;
+        if (this.state.patient_info.patient){
+            return (
+                <div className="Page">
+                    <SideBar/>
 
-                <div className="info-cards-bg">
+                    <div className="info-cards-bg">
 
-                    <div className="title">Patients Overview</div>
+                        <div className="title">Patients Overview</div>
 
-                    <div className="card">
-                        <LargeNameCard
-                            name={this.state.patientInfo.name}
-                            id={this.state.patientInfo.id}
-                            age={this.state.patientInfo.weight}
-                            birthday={this.state.patientInfo.birthday}
-                            className="card"
-                        />
+                        <div className="card">
+                            <LargeNameCard
+                                name={this.state.patient_info.patient.name}
+                                id={this.state.patient_info.patient.id}
+                                age={this.state.patient_info.patient.weight}
+                                birthday={this.state.patient_info.patient.birthday}
+                                className="card"
+                            />
+                        </div>
+
+                        <div className="card">
+                            <NewRecordCard
+                                name={this.state.patient_info.patient.name}
+                                id={this.state.patient_info.patient.id}
+                                age={this.state.patient_info.patient.weight}
+                                birthday={this.state.patient_info.patient.birthday}
+                                className="card"
+                                DoctorUpdateForm={this.doctorUpdate.bind(this)}
+                            />
+                        </div>
+
+                        <div className="card">
+                            <BloodStatLineChart 
+                                title="Blood Suger Changes"
+                                data={this.formatData(this.state.patient_info.blood_sugar, "number")}
+                            />
+                        </div>
+
+                         <div className="card">
+                            <BloodStatLineChart
+                                title="Blood Pressure Low"
+                                data={this.formatData(this.state.patient_info.blood_pressure, "low")}
+                            />
+                        </div>
+
+                        <div className="card">
+                            <BloodStatLineChart
+                                title="Blood Pressure high"
+                                data={this.formatData(this.state.patient_info.blood_pressure, "high")}
+                            />
+                        </div>
+
+                        <div className="card">
+                            <BloodStatLineChart
+                                title="Blood Fat Changes"
+                                data={this.formatData(this.state.patient_info.blood_fat, "number")}
+                                options={{legend: {
+                                    display: false
+                                }}}
+                            />
+                        </div>
+
+                       
+
                     </div>
-
-                    <div className="card">
-                        <NewRecordCard
-                            name={this.state.patientInfo.name}
-                            id={this.state.patientInfo.id}
-                            age={this.state.patientInfo.weight}
-                            birthday={this.state.patientInfo.birthday}
-                            className="card"
-                            DoctorUpdateForm={this.doctorUpdate.bind(this)}
-                        />
+                    <div className="PatientOverview-form" id="updateForm">
+                        <div className="PatientOverview-form-pop" >
+                            <DoctorUpdateForm key = {this.state.uniqueID}
+                                DoctorUpdateForm={this.doctorUpdate.bind(this)}
+                            />
+                        </div>
                     </div>
-
-                    <div className="card">
-                        <BloodStatLineChart 
-                            title="Blood Suger Changes"
-                            data={this.state.bloodSugerData}
-                            options={{legend: {
-                                display: false
-                            }}}
-                        />
-                    </div>
-
-                    <div className="card">
-                        <BloodStatLineChart
-                            title="Blood Pressure Changes"
-                            data={this.state.bloodPressureData}
-                        />
-                    </div>
-
-                    <div className="card">
-                        <BloodStatLineChart
-                            title="Blood Fat Changes"
-                            data={this.state.bloodFatData}
-                            options={{legend: {
-                                display: false
-                            }}}
-                        />
-                    </div>
-
                 </div>
-                <div className="PatientOverview-form" id="updateForm">
-                    <div className="PatientOverview-form-pop" >
-                        <DoctorUpdateForm key = {this.state.uniqueID}
-                            DoctorUpdateForm={this.doctorUpdate.bind(this)}
-                        />
-                    </div>
+            );
+        }
+        else{
+            return(
+                <div>
+                    Loading...
                 </div>
-            </div>
-        );
+            )
+        }
     }
 }
 
